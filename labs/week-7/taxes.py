302c899a67026ca215bcc17108ec039d2978f625
hoonkkim <49007244+hoonkkim@users.noreply.github.com>
2024-10-16 17:05:08 +0000

    lab 7

---
+++ labs/week-7/taxes.py
@@ -0,0 +1,19 @@
+# Part 3. Coding with ChatGPT!
+
+import pandas as pd
+import seaborn as sns
+import os
+
+def task_1():
+
+    return 
+
+
+def task_2():
+
+    return
+
+
+def task_3():
+
+    return
